9b44b4209a3973196d763a43a3648cc673ccb4a3
roagr <roagr@dtu.dk>
2025-07-16 20:15:27 +0200

    Bump version: 0.2.1 → 0.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "coolpropx"
 copyright = "2025, Roberto Agromayor"
 author = ""
-release = "v0.2.1"
+release = "v0.2.2"
 
 # Define extensions
 extensions = [
