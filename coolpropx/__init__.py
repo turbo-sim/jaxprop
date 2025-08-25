445a5db8bf27191472a084151b80db48467a5ec9
roagr <roagr@dtu.dk>
2025-08-25 18:13:20 +0200

    Bump version: 0.2.16 → 0.2.17

--- coolpropx/__init__.py
+++ coolpropx/__init__.py
@@ -24,7 +24,7 @@ from . import jax_import
 
 
 # Package info
-__version__ = "0.2.16"
+__version__ = "0.2.17"
 PACKAGE_NAME = "coolpropx"
 URL_GITHUB = "https://github.com/turbo-sim/coolpropx"
 URL_DOCS = "https://turbo-sim.github.io/coolpropx/"
