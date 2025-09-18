f7cc357fab8fe138af7cb90f5026da218eee543b
roagr <roagr@dtu.dk>
2025-09-18 23:04:46 +0200

    Bump version: 0.4.0 → 0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "jaxprop"
 copyright = "2025, Roberto Agromayor"
 author = ""
-release = "v0.4.0"
+release = "v0.4.1"
 
 # Define extensions
 extensions = [
