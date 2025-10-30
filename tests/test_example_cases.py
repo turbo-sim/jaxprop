84c7d893772dc9bc36bbcbd62004753c2588f3a9
roagr <roagr@dtu.dk>
2025-10-30 10:41:25 +0100

    update tests

--- tests/test_example_cases.py
+++ tests/test_example_cases.py
@@ -12,25 +12,41 @@ EXAMPLES_DIR = THIS_DIR.parent / "examples"
 
 # Manually define relative paths from examples/ root
 example_relative_paths = [
-    "basic_calculations/solver_coolprop.py",
+    # #
+    # "basic_calculations/solver_coolprop.py",
     # "basic_calculations/solver_equilibrium.py",
     # "basic_calculations/solver_metastable.py",
+    # #
     # "contour_plots/compressibility_factor.py",
     # "contour_plots/pressure_isolines.py",
+    #
     # "props_supersaturation/supersaturation_along_isentrope.py",
     # "props_supersaturation/supersaturation_along_isobar.py",
     # "props_supersaturation/supersaturation_along_spinodal.py",
+    #
     # "props_undersaturation/demo_calculations.py",
     # "props_undersaturation/quality_along_isolines.py",
     # "props_undersaturation/subcooling_along_isobar.py",
     # "props_undersaturation/superheating_along_isobar.py",
-    # # "spinodal_lines/spinodal_lines.py",
-    # "spinodal_lines/spinodal_lines_PT.py",
+    # #
+    "spinodal_lines/spinodal_lines.py",
+    "spinodal_lines/spinodal_lines_PT.py",
+    # #
     # "blending_calculations/blending_condensing.py",
     # "blending_calculations/blending_flashing.py",
-    "bicubic_interpolation/demo_interpolation_midpoints.py",
-    "bicubic_interpolation/demo_solver_consistency.py",
-    "bicubic_interpolation/demo_derivatives_jax.py",
+    # #
+    # "perfect_gas/demo_perfect_gas_eos.py",
+    # "perfect_gas/demo_perfect_gas_gradients.py",
+    # "perfect_gas/polytropic_compression.py",
+    # #
+    # "coolprop_jax/coolprop_broadcast.py",
+    # "coolprop_jax/derivative_consistency_1.py",
+    # "coolprop_jax/derivative_consistency_2.py",
+    # "coolprop_jax/derivative_consistency_3.py",
+    # #
+    # "bicubic_interpolation/demo_interpolation_midpoints.py",
+    # "bicubic_interpolation/demo_solver_consistency.py",
+    # "bicubic_interpolation/demo_derivatives_jax.py",
 ]
 
 # Full absolute paths
@@ -38,6 +54,7 @@ EXAMPLE_SCRIPTS = [EXAMPLES_DIR / rel_path for rel_path in example_relative_path
 
 # Optional: make test output more readable
 example_ids = [p.name for p in EXAMPLE_SCRIPTS]
+
 
 @pytest.mark.parametrize("script_path", EXAMPLE_SCRIPTS, ids=example_ids)
 def test_examples(script_path):
@@ -48,7 +65,7 @@ def test_examples(script_path):
         cwd=working_dir,
         capture_output=True,
         text=True,
-        env={**os.environ, "DISABLE_PLOTS": "1"}
+        env={**os.environ, "DISABLE_PLOTS": "1"},
     )
 
     assert result.returncode == 0, f"Failed: {script_path}\n{result.stderr}"
@@ -58,4 +75,3 @@ if __name__ == "__main__":
 
     # Running pytest from this script
     pytest.main([__file__, "-v"])
-
